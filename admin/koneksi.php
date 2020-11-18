3245c0e30b4fbc16255e62601da154c359c69ac0
affrian <42446844+affrian@users.noreply.github.com>
2020-11-18 08:27:08 +0700

    Update koneksi.php

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -1,5 +1,5 @@
 <?php 
 error_reporting(E_ALL^(E_DEPRECATED));
 mysql_connect("localhost","root","") or die (mysql_error());
-mysql_select_db("dawoo_oop") or die(mysql_error());
+mysql_select_db("pt_dawoo") or die(mysql_error());
 ?>
